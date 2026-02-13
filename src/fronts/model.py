8383f522a8d9254e7435cb651c8d36f003bb53fe
mandelbaum.taylor@gmail.com <mandelbaum.taylor@gmail.com>
2026-02-13 17:51:01 -0600

    mid-flight updating model.py; managing interactions between unets and model

--- src/fronts/model.py
+++ src/fronts/model.py
@@ -1,23 +1,36 @@
 import dataclasses
-from typing import Literal, Any
-from fronts.models import unets
+from typing import Literal
+from fronts.models.unets import UNetRegistry
 from fronts.models.keras_builders import (
-    ConvRegularizerConfig,
+    ConvOutputConfig,
     BiasVectorConfig,
-    KernelConfig,
+    KernelMatrixConfig,
     OptimizerConfig,
+    ActivationConfig,
+    LossConfig,
+    MetricConfig,
 )
 
 
 @dataclasses.dataclass
-class CoreUNetConfig:
+class ModelConfig:
+    """Configuration for the model architecture and training parameters."""
+
+    name: Literal[
+        "unet",
+        "unet_ensemble",
+        "unet_plus",
+        "unet_2plus",
+        "unet_3plus",
+        "attention_unet",
+    ]
     loss: LossConfig
     metric: MetricConfig
     optimizer: OptimizerConfig
-    convolution_activity_regularizer: ConvRegularizerConfig
+    convolution_activity_regularizer: ConvOutputConfig
     bias_vector: BiasVectorConfig
-    kernel_matrix: KernelConfig
-    activation: list[str]
+    kernel_matrix: KernelMatrixConfig
+    activation: ActivationConfig
     batch_normalization: bool
     num_filters: list[int]
     kernel_size: list[int]
@@ -28,86 +41,115 @@ class CoreUNetConfig:
     upsample_size: tuple[int]
     bias: bool
 
-    def __post_init__(self):
+    def build(self):
+        """Builds the UNet model based on the configuration."""
+        self.model = UNetRegistry(name=self.name)
+        return self.model(
+            name=self.name,
+            convolution_activity_regularizer=self.convolution_activity_regularizer,
+            bias_vector=self.bias_vector,
+            kernel_matrix=self.kernel_matrix,
+            activation=self.activation,
+            batch_normalization=self.batch_normalization,
+            num_filters=self.num_filters,
+            kernel_size=self.kernel_size,
+            depth=self.depth,
+            modules_per_node=self.modules_per_node,
+            padding=self.padding,
+            pool_size=self.pool_size,
+            upsample_size=self.upsample_size,
+            bias=self.bias,
+        )
+
+
+class Model:
+    def __init__(
+        self,
+        name: Literal[
+            "unet",
+            "unet_ensemble",
+            "unet_plus",
+            "unet_2plus",
+            "unet_3plus",
+            "attention_unet",
+        ],
+        loss_config: LossConfig,
+        metric_config: MetricConfig,
+        optimizer_config: OptimizerConfig,
+        convolution_activity_regularizer_config: ConvOutputConfig,
+        bias_vector_config: BiasVectorConfig,
+        kernel_matrix_config: KernelMatrixConfig,
+        activation_config: ActivationConfig,
+        batch_normalization: bool,
+        num_filters: list[int],
+        kernel_size: list[int],
+        depth: int,
+        modules_per_node: int,
+        padding: Literal["same", "valid"],
+        pool_size: tuple[int],
+        upsample_size: tuple[int],
+        bias: bool,
+    ):
+        self.name = name
+        self.loss_config = loss_config
+        self.metric_config = metric_config
+        self.optimizer_config = optimizer_config
+        self.convolution_activity_regularizer_config = (
+            convolution_activity_regularizer_config
+        )
+        self.bias_vector_config = bias_vector_config
+        self.kernel_matrix_config = kernel_matrix_config
+        self.activation_config = activation_config
+        self.batch_normalization = batch_normalization
+        self.num_filters = num_filters
+        self.kernel_size = kernel_size
+        self.depth = depth
+        self.modules_per_node = modules_per_node
+        self.padding = padding
+        self.pool_size = pool_size
+        self.upsample_size = upsample_size
+        self.bias = bias
+
         if len(self.num_filters) != self.depth:
             raise ValueError(
                 f"Length of num_filters ({len(self.num_filters)}) must match depth "
                 f"({self.depth})"
             )
+        # Build keras objects
+        self.loss = self.loss_config.build()
+        self.metric = self.metric_config.build()
+        self.optimizer = self.optimizer_config.build()
+        self.activity_regularizer = self.convolution_activity_regularizer_config.build()
+        self.bias_vector = self.bias_vector_config.build()
+        self.kernel_matrix = self.kernel_matrix_config.build()
+        self.activation = self.activation_config.build()
+        # TODO: input_shape and num_classes
         # TODO: modify pool size if needed to match len of dims
         # TODO: match kernel size to depth as well
         # TODO: match upsample_size as well
 
-
-@dataclasses.dataclass
-class UNetEnsembleConfig(CoreUNetConfig):
-    num_models: int
-
-
-@dataclasses.dataclass
-class UNetPlusConfig(CoreUNetConfig):
-    deep_supervision: bool
-
-
-@dataclasses.dataclass
-class UNet2PlusConfig:
-    deep_supervision: bool
-
-
-@dataclasses.dataclass
-class UNet3PlusConfig(CoreUNetConfig):
-    deep_supervision: bool
-    num_aggregate_filters: int
-    full_scale_skip_connection_filters: int
-    first_encoder_connections: bool
-
-
-@dataclasses.dataclass
-class AttentionUNetConfig:
-    deep_supervision: bool
-
-    def __post_init__(self):
-        if len(self.upsample_size) > 0:
-            raise ValueError(
-                "AttentionUNet does not support upsample_size, use empty tuple."
+        # Build config to match arg names in the unet functions
+        def build_model(self):
+            """Builds the model based on the configuration."""
+            return self.model.build(
+                input_shape=(
+                    None,
+                    None,
+                    None,
+                    1,
+                ),  # Placeholder, should be set properly
+                num_classes=1,  # Placeholder, should be set properly
+                num_filters=self.num_filters,
+                kernel_size=self.kernel_size,
+                depth=self.depth,
+                modules_per_node=self.modules_per_node,
+                padding=self.padding,
+                pool_size=self.pool_size,
+                upsample_size=self.upsample_size,
+                bias=self.bias,
+                activity_regularizer=self.activity_regularizer,
+                bias_vector=self.bias_vector,
+                kernel_matrix=self.kernel_matrix,
+                activation=self.activation,
+                batch_normalization=self.batch_normalization,
             )
-
-
-@dataclasses.dataclass
-class UNetRegistry:
-    model: Literal[
-        "unet",
-        "unet_ensemble",
-        "unet_plus",
-        "unet_2plus",
-        "unet_3plus",
-        "attention_unet",
-    ]
-    config: dict[str, Any]
-
-    def build(self):
-        match self.model:
-            case "unet":
-                from .unet import UNet
-
-                return UNet(self.config)
-            case "unet_ensemble":
-                from .unet_ensemble import UNetEnsemble
-
-                return UNetEnsemble(self.config)
-            case "unet_plus":
-                from .unet_plus import UNetPlus
-
-                return UNetPlus(self.config)
-            case "unet_2plus":
-                from .unet_2plus import UNet2Plus
-
-                return UNet2Plus(self.config)
-            case "unet_3plus":
-                from .unet_3plus import UNet3Plus
-
-                return UNet3Plus(self.config)
-            case "attention_unet":
-                from .attention_unet import AttentionUNet
-
-                return AttentionUNet(self.config)
