6b29b207419c9a1dd7a28452e3d542fa37c9c53d
mandelbaum.taylor@gmail.com <mandelbaum.taylor@gmail.com>
2026-03-11 20:11:59 -0500

    rename model data to model training data for clarity, remove fronts data config from config.py

--- src/fronts/data/targets.py
+++ src/fronts/data/targets.py
@@ -1,10 +1,14 @@
 import icechunk
 import xarray as xr
 import dataclasses
+import logging
+from fronts.utils import data_utils
+
+log = logging.getLogger("fronts.data.targets")
 
 
 @dataclasses.dataclasses
-class TargetData:
+class TargetDataConfig:
     """Dataclass to hold the information about the target data and build it.
 
     Attributes:
@@ -13,6 +17,8 @@ class TargetData:
     """
 
     store_path: str
+    front_types: str | list[str]
+    front_dilation: int
 
     def build(self) -> xr.Dataset:
         """Build the target dataset from the icechunk store.
@@ -33,4 +39,19 @@ class TargetData:
             consolidated=False,
             chunks={},
         )
+
+        # Reformat the fronts if front_types is specified.
+        if self.front_types is not None:
+            log.debug("Reformatting fronts with front_types=%s...", self.front_types)
+            ds = data_utils.reformat_fronts(ds, self.front_types)
+            log.debug("reformat_fronts complete.")
+
+        # Dilate the fronts if set to > 0
+        if self.front_dilation > 0:
+            log.debug(
+                "Expanding fronts with %d dilation iteration(s)...", self.front_dilation
+            )
+            ds = data_utils.expand_fronts(ds, iterations=self.front_dilation)
+            log.debug("expand_fronts complete.")
+
         return ds
